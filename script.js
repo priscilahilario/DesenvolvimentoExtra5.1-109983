5271ed902d9b44c742270eb1f2b9046713daea71
Priscila Hilario <hilario.prii@gmail.com>
2024-07-22 10:14:54 -0300

    desenvolvimento extra 5.2

---
+++ script.js
@@ -0,0 +1,39 @@
+
+let elementoA = document.querySelector('a');
+console.log(elementoA);
+console.log(elementoA.innerText);
+elementoA.innerText=`Proz-Educação`;
+
+let elementoH1 = document.querySelector('h1');
+console.log(elementoH1);
+console.log (elementoH1.innerText);
+elementoH1.innerText=`Desenvolvimento 5.2`;
+
+
+/*let elementoLi = document.querySelector('Li');
+console.log(elementoLi);
+console.log(elementoLi.innerHTML);
+elementoLi.innerHTML = `Listas` */
+
+let elementoOl = document.querySelector('ol');
+console.log(elementoOl);
+console.log(elementoOl.innerText);
+elementoOl.innerHTML = `<h2>Lista ordenada</h2>
+<ol>
+<li>Elemento de lista JS 01</li>
+<li>Elemento de lista JS 02</li>
+<li>Elemento de lista JS 03</li>
+</ol>`
+
+let elementoUl = document.querySelector('ul');
+console.log(elementoUl);
+console.log(elementoUl.innerHTML);
+elementoUl.innerHTML= `<h3>Lista não ordenada</h3>
+<ul>
+<li>Elemento de lista JS 01</li>
+<li>Elemento de lista JS 02</li>
+<li>Elemento de lista JS 03</li>
+</ul>
+`
+
+ 
